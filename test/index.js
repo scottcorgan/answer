5835fd131e45d1cd89805c453a184c3070fc8929
Scott Corgan <scottcorgan@gmail.com>
2014-06-18 17:09:00 -0700

    tests: Test coverage for res.set

--- test/index.js
+++ test/index.js
@@ -93,6 +93,72 @@ describe('answer', function () {
         .expect('123')
         .end(done);
     });
+    
+    it('should set multiple response header fields', function (done) {
+      app.use(function(req, res){
+        res.set('Set-Cookie', ["type=ninja", "language=javascript"]);
+        res.send(res.get('Set-Cookie'));
+      });
+
+      request(app)
+        .get('/')
+        .expect('["type=ninja","language=javascript"]', done);
+    });
+    
+    it('should coerce to an array of strings', function (done) {
+      app.use(function (req, res) {
+        res.headers = {};
+        res.set('X-Numbers', [123, 456]);
+        res.end(JSON.stringify(res.get('X-Numbers')));
+      });
+      
+      request(app)
+        .get('/')
+        .expect('["123","456"]')
+        .end(done);
+    });
+    
+    it('should not set a charset of one is already set', function (done) {
+      app.use(function (req, res) {
+        res.headers = {};
+        res.set('Content-Type', 'text/html; charset=lol');
+        res.end(res.get('content-type'));
+      });
+      
+      request(app)
+        .get('/')
+        .expect('text/html; charset=lol')
+        .end(done);
+    });
+    
+    it('should set multiple fields', function (done) {
+      app.use(function(req, res){
+        res.set({
+          'X-Foo': 'bar',
+          'X-Bar': 'baz'
+        }).end();
+      });
+
+      request(app)
+        .get('/')
+        .expect('X-Foo', 'bar')
+        .expect('X-Bar', 'baz')
+        .end(done);
+    });
+    
+    it('should coerce to a string', function (done) {
+      app.use(function (req, res) {
+        res.headers = {};
+        res.set({ 'X-Number': 123 });
+        res.end(res.get('X-Number'));
+      });
+      
+      request(app)
+        .get('/')
+        .expect('123')
+        .end(done);
+    });
+    
   });
   
 });
